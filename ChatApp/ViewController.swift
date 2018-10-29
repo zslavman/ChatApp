258926ce09a7acde3fb568231feaa45b55476d39
Zinko Vyacheslav <zslavman@gmail.com>
2018-10-29 10:39:35 +0200

    Initial Commit

---
+++ ChatApp/ViewController.swift
@@ -0,0 +1,31 @@
+//
+//  ViewController.swift
+//  ChatApp
+//
+//  Created by Zinko Vyacheslav on 29.10.2018.
+//  Copyright © 2018 Zinko Vyacheslav. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UITableViewController {
+
+	
+	
+	
+	override func viewDidLoad() {
+		super.viewDidLoad()
+		
+		navigationItem.leftBarButtonItem = UIBarButtonItem(title: "Logout", style: .plain, target: self, action: #selector(onLogout))
+		
+	}
+
+
+	
+	@objc private func onLogout(){
+		
+	}
+
+
+}
+
